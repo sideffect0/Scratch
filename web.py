935ab348d40c27d5286324600e948fa7280c3f3e
sideffect0 <mail3renjith@gmail.com>
2016-07-28 10:16:09 +0530

    add Scratch start page

--- web.py
+++ web.py
@@ -1,2 +1,8 @@
-def application(*args, **kwargs):
-    pass
+def application(environ, start_response):
+    status = '200 OK'
+    body = b'Scratch Works: Hack and Learn'
+
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(body)))]
+    start_response(status, response_headers)
+    return [body]
